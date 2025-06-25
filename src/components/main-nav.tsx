57b764cef7ef4b7dd0b90c23c72fa0a6bf1e1432
Rodolpho Beckman <rodolpho.beckman@gmail.com>
2025-06-25 17:49:21 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -57,18 +57,20 @@ export function MainNav() {
         <SidebarMenu>
           {menuItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <SidebarMenuButton
-                asChild
-                isActive={pathname === item.href}
-                tooltip={{
-                  children: item.label,
-                }}
-              >
-                <Link href={item.href}>
-                  <item.icon />
-                  <span>{item.label}</span>
-                </Link>
-              </SidebarMenuButton>
+              <Link href={item.href} passHref legacyBehavior>
+                <SidebarMenuButton
+                  asChild
+                  isActive={pathname === item.href}
+                  tooltip={{
+                    children: item.label,
+                  }}
+                >
+                  <a>
+                    <item.icon />
+                    <span>{item.label}</span>
+                  </a>
+                </SidebarMenuButton>
+              </Link>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
