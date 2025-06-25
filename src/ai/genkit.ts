09b227b7c8e71e77ede8883d0b1e50b291f69e40
Rodolpho Beckman <rodolpho.beckman@gmail.com>
2025-06-25 23:59:12 +0000

    erro

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,11 @@
-import { config } from 'dotenv';
-config();
-
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GOOGLE_API_KEY,
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
